dadc0bed5d3c251f1c6c6c1ee4972aa32184f177
MatheusN15 <matheusmn1519@gmail.com>
2021-07-15 11:28:11 -0300

    initial commit

---
+++ src/main/java/br/com/matheus/helpDesk/enums/ProfileEnum.java
@@ -0,0 +1,7 @@
+package br.com.matheus.helpDesk.enums;
+
+public enum ProfileEnum {
+	ROLE_ADMIN,
+	ROLE_CUSTUMER,
+	ROLE_TECHNICIAN
+}
---
+++ src/main/java/br/com/matheus/helpDesk/security/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package br.com.matheus.helpDesk.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.csrf().disable().authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/user").permitAll()
+		.antMatchers(HttpMethod.POST, "/user").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin().permitAll()
+		.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication()
+		.withUser("klay").password("123").roles("ADMIN");
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception{
+		web.ignoring().antMatchers("/style/**");
+	}
+	
+	
+}
